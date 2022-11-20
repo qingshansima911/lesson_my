0bbe3f7f9acc2517f126c03af20c3bdda726d9c9
qingshansima <118155704+qingshansima911@users.noreply.github.com>
2022-11-20 14:43:55 +0800

    大路虎suv

--- dlh/miniprogram/pages/index/index.ts
+++ dlh/miniprogram/pages/index/index.ts
@@ -4,12 +4,39 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    slides: [
+      {
+        "id": 5,
+        "header": "全新一代发现",
+        "sub_header": "Discovery",
+        "description": "全尺寸七座 SUV，现已接受预订。",
+        "image": "https://resources.ninghao.net/landrover/discover-1.jpg"
+      },
+      {
+        "id": 3,
+        "header": "全新揽胜星脉",
+        "sub_header": "Range Rover",
+        "description": "标新立异的前卫揽胜。",
+        "image": "https://resources.ninghao.net/landrover/velar-1.jpg"
+      },
+      {
+        "id": 6,
+        "header": "发现神行",
+        "sub_header": "Discovery",
+        "description": "发现的绝佳时刻。",
+        "image": "https://resources.ninghao.net/landrover/discovery-sport-1.jpg"
+      }
+    ]
+  },
+  testDrive() {
+    console.log("试驾一下，百公里2.9s");
+  },
+  readMore(event: any) {
+    let id = event.target.dataset.id;
+    // console.log(id);
+    wx.navigateTo({
+      url: `/pages/vehicles/show?id=${id}`,
+    })
   },
   // 事件处理函数
   bindViewTap() {
