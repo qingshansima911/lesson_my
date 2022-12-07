abeb6253f17bcf70759ff20b47bf8cfc38452c65
qingshansima911 <3499424491@qq.com>
2022-12-07 21:34:18 +0800

    js执行机制，单线程

---
+++ weapp/mall/pages/index/index.js
@@ -0,0 +1,66 @@
+const WXAPI = require("apifm-wxapi");
+Page({
+  data:{
+    search:'',
+    activeCategory:0,
+    categories:[], /* 分类列表 */
+    currentGoods:[] /* 当前商品列表 */
+  },
+  onLoad(){
+    // 分享
+    // wx.showShareMenu({
+    //   withShareTicket:ticket
+    // })
+    // 面向对象 分离
+    this.categories();
+  },
+  // 分离请求数据
+  categories(){
+    wx.showLoading({
+      title:"加载中..."
+    })
+    WXAPI.goodsCategory()
+    .then(res=>{
+      console.log(res);
+      wx.hideLoading();
+      // 没有发生错误
+      if(res.code==0){
+        const categories = res.data;
+        this.setData({
+          categories
+        })
+      }
+    })
+  },
+  
+  bindconfirm(e){
+    console.log(e,e.detail);
+    let search = e.detail;
+    this.setData({
+      search
+    })
+    wx.navigateTo({
+      url: `/pages/goods/list?name=${search}`,
+    })
+  },
+  onCategoryClick(event) {
+    const idx = event.target.dataset.idx
+    console.log(idx, '/////')
+    this.setData({
+      activeCategory: idx
+    })
+  },
+  searchscan(){
+    wx.scanCode({
+      scanType:["barCode","qrCode"],
+      success:res=>{
+        this.setData({
+          search:res.result
+        })
+        wx.navigateTo({
+          url: `/pages/goods/list?name=${res.result}`,
+        })
+      }
+    })
+  }
+})
