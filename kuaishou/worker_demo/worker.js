70f1acdb6af2dd5f95aa8ebb79b227ceb5adb99f
qingshansima <3499424491@qq.com>
2023-07-28 20:22:13 +0800

    worker_demo

---
+++ kuaishou/worker_demo/worker.js
@@ -0,0 +1,20 @@
+// worker 不能操作 DOM 只能跟页面发消息
+self.addEventListener('message', (event) => {
+    const n = event.data;
+    const result = fibonacci(n);
+    self.postMessage({ inputNumber: n, result })
+})
+
+function fibonacci(n) {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+    let a = 0;
+    let b = 1;
+    for (let i = 2; i <= n; i++) {
+        const c = a + b;
+        a = b;
+        b = c;
+    }
+    return b.toString();
+}
