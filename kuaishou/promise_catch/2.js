f6a7af2c0491015218b874e7963a5c53f4e9c9ce
qingshansima <3499424491@qq.com>
2023-07-28 20:21:47 +0800

    promise

---
+++ kuaishou/promise_catch/2.js
@@ -0,0 +1,9 @@
+Promise.resolve()
+    .then(function () {
+        throw new Error('外部错误')
+    }, function () {
+        console.log('This is never called');
+    })
+    .catch(err => {
+        console.log('捕捉到错误', err.message);
+    })
