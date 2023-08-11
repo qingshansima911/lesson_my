ed7340470a77adcc855eb3789ca4d76b8f0f7081
qingshansima <3499424491@qq.com>
2023-08-11 23:29:26 +0800

    ts-cart

--- vue-ts/ts-cart/src/store/cart.ts
+++ vue-ts/ts-cart/src/store/cart.ts
@@ -3,38 +3,38 @@ import { IProduct } from "../service/shop";
 import { defineStore } from "pinia";
 import { useProductsStore } from "./products";
 
-export interface ICatProducts{
-    id: number
-    title: string
-    price: number
-    quantity: number
+export interface ICatProducts {
+  id: number
+  title: string
+  price: number
+  quantity: number
 }
 export const useCartStore = defineStore('cart', () => {
-    const state = reactive({
-        cartProducts: [] as ICatProducts[],
-        checkoutStatus: null
-    })
-    // 没有定义的变量要删掉
-    const addProductToCart = (product:IProduct) => {
-        if (product.inventory < 1) {
-            return   
-        }
-        const cartItem = state.cartProducts.find(item => item.id === product.id)
-        if (cartItem) {
-            cartItem.quantity++
-        } else {
-            state.cartProducts.push({
-                id: product.id,
-                quantity: 1,
-                title: product.title,
-                price: product.price
-            })
-        }
-        const productStore = useProductsStore()
-        productStore.decrementProduct(product)
+  const state = reactive({
+    cartProducts: [] as ICatProducts[],
+    checkoutStatus: null
+  })
+  // 没有定义的变量要删掉
+  const addProductToCart = (product: IProduct) => {
+    if (product.inventory < 1) {
+      return
     }
-    return {
-        state,
-        addProductToCart
+    const cartItem = state.cartProducts.find(item => item.id === product.id)
+    if (cartItem) {
+      cartItem.quantity++
+    } else {
+      state.cartProducts.push({
+        id: product.id,
+        quantity: 1,
+        title: product.title,
+        price: product.price
+      })
     }
+    const productStore = useProductsStore()
+    productStore.decrementProduct(product)
+  }
+  return {
+    state,
+    addProductToCart
+  }
 })
