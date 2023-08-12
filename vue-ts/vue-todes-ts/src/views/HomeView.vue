ae02a32a5248281a65b01987d6e9a05ea7bc0d14
qingshansima <3499424491@qq.com>
2023-08-12 23:05:46 +0800

    vue-todes-ts

--- vue-ts/vue-todes-ts/src/views/HomeView.vue
+++ vue-ts/vue-todes-ts/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
-    <div>
-        <!-- html5 语义化 -->
-        <main>
-            <h1>Todos</h1>
-            <form class="addForm" @submit.prevent="addTodo">
-                <label for="add">Add todo</label>
-                <div class="sl">
-                    <input type="text" name="add" id="add" v-model="currentTodoInp">
-                    <button type="submit">Add</button>
-                </div>
-            </form>
-        </main>
-    </div>
+  <div>
+    <!-- html5 语义化 -->
+    <main>
+      <h1>Todos</h1>
+      <form class="addForm" @submit.prevent="addTodo">
+        <label for="add">Add todo</label>
+        <div class="sl">
+          <input type="text" name="add" id="add" v-model="currentTodoInp">
+          <button type="submit">Add</button>
+        </div>
+      </form>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -24,11 +24,11 @@ const todoStore = useTodoStore()
 // const productsStore = useProductsStore()
 todoStore.initFromLocalStorage() // 从localStorage 拿出来并更新到store 中 
 const addTodo = () => {
-    const text = currentTodoInp.value
-    currentTodoInp.value = ''
-    if (text.trim() !== '') { // 必须输入值,不能只是空格
-        todoStore.addTodo(text)
-    }
+  const text = currentTodoInp.value
+  currentTodoInp.value = ''
+  if (text.trim() !== '') { // 必须输入值,不能只是空格
+    todoStore.addTodo(text)
+  }
 }
 
 // todoStore.addTodo('十天后面试')
