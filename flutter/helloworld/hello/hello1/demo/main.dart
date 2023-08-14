81427b6ac780c7abf94b63f3e58eb8fa7c11547f
qingshansima <3499424491@qq.com>
2023-08-14 13:32:21 +0800

    flutter

---
+++ flutter/helloworld/hello/hello1/demo/main.dart
@@ -0,0 +1,4 @@
+void main() {
+  var t = 'a';
+  print(t);
+}
---
+++ flutter/helloworld/helloworld/android/app/src/main/kotlin/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.helloworld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
